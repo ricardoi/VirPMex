49952455cc27fd12f9b65111533b7de2f8686625
Ricardo I <ralcala@ufl.edu>
2021-08-30 22:05:39 -0700

    Update host_genomeseqs-removal.sh

--- scripts/host_genomeseqs-removal.sh
+++ scripts/host_genomeseqs-removal.sh
@@ -1,17 +1,21 @@
-#Remove host machote
-#Tutorial http://www.metagenomics.wiki/tools/samtools/remove-host-sequences
+# Remove Host Sequence from Viromes
 
-#Map againt host
-#Cpapaya is going to be your index file 
+# Tutorial http://www.metagenomics.wiki/tools/samtools/remove-host-sequences
 
-bowtie2-build genome.fna Cpapaya
+# Map Illumina reads againt host genome sequence
+# with bowtie2 https://www.nature.com/articles/nmeth.1923
+module load bowtie2 samtools bedtools
 
-bowtie2 -x Cpapaya -1 Reads_1.fastq -2 Reads_2.fastq -S SAMPLE_mapped_and_unmapped.sam
-samtools view -bS SAMPLE_mapped_and_unmapped.sam > SAMPLE_mapped_and_unmapped.bam
+# Index your Host file 
+bowtie2-build genome.fna Host # takes about several minutes
 
-#Filter unmapped reads
-samtools view -b -f 12 -F 256 SAMPLE_mapped_and_unmapped.bam > SAMPLE_bothEndsUnmapped.bam
+# Mapping reads to the Host genome 
+bowtie2 -x Host -U Reads_1.fastq Reads_2.fastq -S host_mapped_and_unmapped.sam
+samtools view -bS host_mapped_and_unmapped.sam > host_mapped_and_unmapped.bam
 
-#Split pair-end reads into separated fastq files...
- samtools sort -n SAMPLE_bothEndsUnmapped.bam SAMPLE_bothEndsUnmapped_sorted 
- bedtools bamtofastq -i SAMPLE_bothEndsUnmapped_sorted.bam -fq SAMPLE_r1.fastq -fq2 SAMPLE_r2.fastq
+# Filter unmapped reads
+samtools view -b -f 12 -F 256 host_mapped-unmapped.sam > host_both-pairs_unmapped.bam
+
+# Split pair-end reads into separated fastq files...
+samtools sort -n host_both-pairs_unmapped.bam -o host_bothEndsUnmapped_sorted.bam 
+bedtools bamtofastq -i host_both-pairs_unmapped_sorted.bam -fq host_filtered.r1.fastq -fq2 host_filtered.r2.fastq
